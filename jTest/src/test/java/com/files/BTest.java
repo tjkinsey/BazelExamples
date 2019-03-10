f34bc7ebb36e25d9ab6443a7f1511a96cd9f3a64
tjkinsey <tjkinsey08@gmail.com>
2019-03-10 12:05:32 -0600

    Added to the junit tests

--- jTest/src/test/java/com/files/BTest.java
+++ jTest/src/test/java/com/files/BTest.java
@@ -1,26 +1,47 @@
 package test.java.com.files;
 import main.java.com.files.B;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
 
+@RunWith(JUnit4.class)
 public class BTest{
     
     B b1;
     B b2;
+    B b3;
 
     @Before
     public void init(){
         b1 = new B("Tanner", "Kinsey", 29);
         b2 = new B("Amanda","Kinsey", 27);
+        b3 = new B("Test", "Old", 1000);
     }
 
     @Test
-    public void test1(){
-        assertTrue(true);
-        assertFalse(b2.isOld());
+    public void testGetFirstName(){
+        assertEquals("Tanner", b1.getFirstName());
     }
 
+    @Test
+    public void testGetLastName(){
+        assertEquals("Kinsey", b2.getLastName());
+    }
 
+    @Test
+    public void testGetAge(){
+        assertEquals(27, b2.getAge());
+    }
+
+    @Test
+    public void testIsOldFalse(){
+        assertFalse(b1.isOld());
+    }
+
+    @Test
+    public void testIsOldTrue(){
+        assertTrue(b3.isOld());
+    }
 }
--- jTest/src/test/java/com/files/CTest.java
+++ jTest/src/test/java/com/files/CTest.java
@@ -1,9 +1,12 @@
 package test.java.com.files;
 import main.java.com.files.C;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
 
+@RunWith(JUnit4.class)
 public class CTest{
 
     C c1;
@@ -16,14 +19,22 @@ public class CTest{
     }
 
     @Test
-    public void testCool(){
+    public void testCoolC1(){
         assertTrue(c1.isCool());
+    }
+
+    @Test
+    public void testCoolC2(){
         assertFalse(c2.isCool());
     }
 
     @Test
-    public void getYear(){
+    public void getYearC1(){
         assertEquals(1965, c1.getYear());
-        assertFalse(c2.getYear() == 2000);
+    }
+
+    @Test
+    public void getYearC2(){
+        assertEquals(2006, c2.getYear());
     }
 }
